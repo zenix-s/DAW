04ed467323ae6f73bb52defc6fd9088336b62c1c
zenix-s <sergiofs19@gmail.com>
2024-03-01 10:27:34 +0100

    Add ResponseEntity to getPedidosComercial method

--- DWES/SFF_AD_4_RestApiPedidos/src/main/java/zenixs/ad4/restcontroller/PedidoRestController.java
+++ DWES/SFF_AD_4_RestApiPedidos/src/main/java/zenixs/ad4/restcontroller/PedidoRestController.java
@@ -3,6 +3,7 @@ package zenixs.ad4.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +25,16 @@ public class PedidoRestController {
 	 * @param idComercial id del comercial
 	 * @return List<Pedido>
 	 */
+	// @GetMapping("/comercial/pedidos/{idComercial}")
+	// public List<Pedido> getPedidosComercial(@PathVariable int idComercial) {
+	// 	return pedidoService.getPedidosComercial(idComercial);
+	// }
 	@GetMapping("/comercial/pedidos/{idComercial}")
-	public List<Pedido> getPedidosComercial(@PathVariable int idComercial) {
-		return pedidoService.getPedidosComercial(idComercial);
+	public ResponseEntity<List<Pedido>> getPedidosComercial(@PathVariable int idComercial) {
+		List<Pedido> pedidos = pedidoService.getPedidosComercial(idComercial);
+		if (pedidos.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.status(200).body(pedidos);
 	}
 }
